260c6b4a61459c1e1854ff8d46f4199027af33a7
digitalLumberjack <digitallumberjack@gmail.com>
2017-01-10 23:06:56 +0100

    fixed version in recalbox-patch-target.sh
        the script now used the given version, the date is not added to recalbox.version anymore

--- board/recalbox/recalbox-patch-target.sh
+++ board/recalbox/recalbox-patch-target.sh
@@ -68,15 +68,14 @@ ln -sf "/run/recalbox.shadow" "${TARGET_DIR}/etc/shadow" || exit 1
 
 # Add the date while the version can be nightly or unstable
 RVERSION=$(cat "${TARGET_DIR}/recalbox/recalbox.version")
-echo "${RVERSION} "$(date "+%Y/%m/%d %H:%M") > "${TARGET_DIR}/recalbox/recalbox.version"
 
 # bootsplash
-TGVERSION=$(cat "${TARGET_DIR}/recalbox/recalbox.version")
+TGVERSION="${RVERSION} $(date "+%Y/%m/%d %H:%M")"
 convert "${TARGET_DIR}/recalbox/system/resources/splash/logo.png" -fill white -pointsize 30 -annotate +50+1020 "${TGVERSION}" "${TARGET_DIR}/recalbox/system/resources/splash/logo-version.png" || exit 1
 convert "${TARGET_DIR}/recalbox/system/resources/splash/logo.png" -fill white -pointsize 60 -gravity center -annotate +0+0 "Upgrading the system\nPlease wait..." "${TARGET_DIR}/recalbox/system/resources/splash/logo-upgrade.png" || exit 1
 
 # Splash video subtitle
-echo -e "1\n00:00:00,000 --> 00:00:02,000\n${RVERSION} "$(date "+%Y/%m/%d %H:%M") > "${TARGET_DIR}/recalbox/system/resources/splash/recalboxintro.srt"
+echo -e "1\n00:00:00,000 --> 00:00:02,000\n${TGVERSION}" > "${TARGET_DIR}/recalbox/system/resources/splash/recalboxintro.srt"
 omx_fnt="/usr/share/fonts/dejavu/DejaVuSans-BoldOblique.ttf"
 if [[ -f ${TARGET_DIR}$omx_fnt ]] ; then
 	sed -i "s|omx_fnt=\"\"|omx_fnt=\"--font=$omx_fnt\"|g" "${TARGET_DIR}/etc/init.d/S02splash"
