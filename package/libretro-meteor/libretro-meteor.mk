7f7a423b3dbc062295c59db6acd0871a73bb3eb9
paradadf <paradadf@users.noreply.github.com>
2017-01-14 17:56:13 +0100

    Update libretro-meteor.mk

--- package/libretro-meteor/libretro-meteor.mk
+++ package/libretro-meteor/libretro-meteor.mk
@@ -3,13 +3,13 @@
 # METEOR
 #
 ################################################################################
-LIBRETRO_METEOR_VERSION = b4e749b1f2eb945661f1542b92eb515dd535746c
+LIBRETRO_METEOR_VERSION = a309da72494dc6c0c4db2bead873913ac3e0a815
 LIBRETRO_METEOR_SITE = $(call github,libretro,meteor-libretro,$(LIBRETRO_METEOR_VERSION))
 
 
 define LIBRETRO_METEOR_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
-	$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" AR="$(TARGET_AR)" RANLIB="$(TARGET_RANLIB)" -C $(@D)/libretro platform="$(LIBRETRO_PLATFORM)"
+	$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" AR="$(TARGET_AR)" RANLIB="$(TARGET_RANLIB)" -C $(@D)/libretro platform="$(LIBRETRO_BOARD)"
 endef
 
 define LIBRETRO_METEOR_INSTALL_TARGET_CMDS
