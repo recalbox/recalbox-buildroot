9235007d820ee2918b8fe9e95c20126ef61f0a05
paradadf <paradadf@users.noreply.github.com>
2017-01-14 17:22:09 +0100

    Update libretro-catsfc.mk

--- package/libretro-catsfc/libretro-catsfc.mk
+++ package/libretro-catsfc/libretro-catsfc.mk
@@ -3,15 +3,15 @@
 # CATSFC
 #
 ################################################################################
-LIBRETRO_CATSFC_VERSION = 361b6927ef6af4e0913f18909cb841fd192e69d1
-LIBRETRO_CATSFC_SITE = $(call github,libretro,CATSFC-libretro,$(LIBRETRO_CATSFC_VERSION))
+LIBRETRO_CATSFC_VERSION = a25a25f9a1327a996fe4ff37d3bfb7e3c15d174e
+LIBRETRO_CATSFC_SITE = $(call github,libretro,snes9x2005,$(LIBRETRO_CATSFC_VERSION))
 
 define LIBRETRO_CATSFC_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(LIBRETRO_PLATFORM)"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(LIBRETRO_BOARD)"
 endef
 
 define LIBRETRO_CATSFC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/catsfc_libretro.so \
+	$(INSTALL) -D $(@D)/snes9x2005_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/catsfc_libretro.so
 endef
 
