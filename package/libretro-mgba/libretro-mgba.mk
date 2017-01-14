5d0f982d262c28d9bc75e2f5672767df9d28f68c
paradadf <paradadf@users.noreply.github.com>
2017-01-14 17:57:06 +0100

    Update libretro-mgba.mk

--- package/libretro-mgba/libretro-mgba.mk
+++ package/libretro-mgba/libretro-mgba.mk
@@ -3,7 +3,7 @@
 # MGBA
 #
 ################################################################################
-LIBRETRO_MGBA_VERSION = 851dbb1b5138b2258168cb3c08a740e04313c409
+LIBRETRO_MGBA_VERSION = 4000128339b535896615c994cafcd777637573f4
 LIBRETRO_MGBA_SITE = $(call github,libretro,mgba,$(LIBRETRO_MGBA_VERSION))
 
 ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
@@ -13,7 +13,7 @@ else
 endif
 
 define LIBRETRO_MGBA_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)" $(LIBRETRO_MGBA_NEON)
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_BOARD)" $(LIBRETRO_MGBA_NEON)
 endef
 
 define LIBRETRO_MGBA_INSTALL_TARGET_CMDS
