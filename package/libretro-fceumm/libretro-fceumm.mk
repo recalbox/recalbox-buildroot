ef16f1f5088bcb2607bb51f934b8c00dff52b86b
paradadf <paradadf@users.noreply.github.com>
2017-01-14 17:27:56 +0100

    Update libretro-fceumm.mk

--- package/libretro-fceumm/libretro-fceumm.mk
+++ package/libretro-fceumm/libretro-fceumm.mk
@@ -3,11 +3,11 @@
 # FCEUMM
 #
 ################################################################################
-LIBRETRO_FCEUMM_VERSION = 68f07514c69183b8f3280c86e1d5faca619b5f85
+LIBRETRO_FCEUMM_VERSION = 7fb6b0485a7935eb85354b76d165b1804c22c4cc
 LIBRETRO_FCEUMM_SITE = $(call github,libretro,libretro-fceumm,$(LIBRETRO_FCEUMM_VERSION))
 
 define LIBRETRO_FCEUMM_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_BOARD)"
 endef
 
 define LIBRETRO_FCEUMM_INSTALL_TARGET_CMDS
