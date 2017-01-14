a2aa9051314f9a7b192581c3a4c36eb3c9490724
paradadf <paradadf@users.noreply.github.com>
2017-01-14 18:13:23 +0100

    Update libretro-scummvm.mk

--- package/libretro-scummvm/libretro-scummvm.mk
+++ package/libretro-scummvm/libretro-scummvm.mk
@@ -3,7 +3,7 @@
 # SCUMMVM
 #
 ################################################################################
-LIBRETRO_SCUMMVM_VERSION = dce274dc1aa29e7ef0c4cf6b806e882371089ca5
+LIBRETRO_SCUMMVM_VERSION = c9730958e069155cfbfa3bc519502895044ca8f3
 LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,$(LIBRETRO_SCUMMVM_VERSION))
 #CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
 #	LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR) cru"  \
@@ -11,7 +11,7 @@ LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,$(LIBRETRO_SCUMMVM_VERSIO
 define LIBRETRO_SCUMMVM_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" LDFLAGS="-shared -Wl,--no-undefined" $(MAKE) \
 	TOOLSET="$(TARGET_CROSS)" \
-	-C $(@D)/backends/platform/libretro/build platform="$(LIBRETRO_PLATFORM)"
+	-C $(@D)/backends/platform/libretro/build platform="$(LIBRETRO_BOARD)"
 endef
 
 define LIBRETRO_SCUMMVM_INSTALL_TARGET_CMDS
