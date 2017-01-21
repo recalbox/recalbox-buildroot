65b8100a2e758721948eb20ddff1ad54654a47e3
Subs <frog2wah@gmail.com>
2017-01-21 22:31:59 +0100

    S11share: sha1 check root abd boot for a secure update recalbox-upgrade.sh: add root.list + must have sha1 to upgrade

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-upgrade.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-upgrade.sh
@@ -7,6 +7,11 @@ arch=$(cat /recalbox/recalbox.arch)
 majorversion=4
 updatetype="`$systemsetting  -command load -key updates.type`"
 
+function cancelUpgrade() {
+    rm -rf /recalbox/share/system/upgrade/*
+    exit 1
+}
+
 if test "${updatetype}" != "stable" -a "${updatetype}" != "unstable" -a "${updatetype}" != "beta"
 then
     # force a default value in case the value is removed or miswritten
@@ -29,29 +34,20 @@ then
     exit 1
 fi
 
-if ! mv /recalbox/share/system/upgrade/boot.tar.xz.part /recalbox/share/system/upgrade/boot.tar.xz
-then
-    rm /recalbox/share/system/upgrade/boot.tar.xz.part
-    rm /recalbox/share/system/upgrade/root.tar.xz.part
-    exit 1
-fi
+mv /recalbox/share/system/upgrade/boot.tar.xz.part /recalbox/share/system/upgrade/boot.tar.xz || cancelUpgrade
+mv /recalbox/share/system/upgrade/root.tar.xz.part /recalbox/share/system/upgrade/root.tar.xz || cancelUpgrade
 
-if ! mv /recalbox/share/system/upgrade/root.tar.xz.part /recalbox/share/system/upgrade/root.tar.xz
-then
-    rm /recalbox/share/system/upgrade/boot.tar.xz
-    rm /recalbox/share/system/upgrade/root.tar.xz.part
-    exit 1
-fi
+wget "${recalboxupdateurl}/${majorversion}/${arch}/${updatetype}/last/root.list" -O /recalbox/share/system/upgrade/root.list || cancelUpgrade
+
 
 for file in root.tar.xz boot.tar.xz; do
     # the file may not have a .sha1, so skip in that case
-    wget ${recalboxupdateurl}/${majorversion}/${arch}/${updatetype}/last/${file}.sha1 -O /recalbox/share/system/upgrade/${file}.sha1 || continue
+    wget ${recalboxupdateurl}/${majorversion}/${arch}/${updatetype}/last/${file}.sha1 -O /recalbox/share/system/upgrade/${file}.sha1 || cancelUpgrade
     computedSum=`sha1sum /recalbox/share/system/upgrade/${file} | cut -d ' ' -f 1`
     buildSum=`cat /recalbox/share/system/upgrade/${file}.sha1 | cut -d ' ' -f 1`
     if [[ $computedSum != $buildSum ]]; then
         recallog -e "Checksums differ for ${file}. Aborting upgrade !"
-        rm -rf /recalbox/share/system/upgrade/*
-        exit 8
+        cancelUpgrade
     fi
 done
 
