14fb8f50c4b57f343ad951aa4dd4e0a3259d656a
magicseb <jeremiah252@hotmail.com>
2017-02-07 22:24:42 +0100

    Update RGB-Pi to the latest version
    Update RGB-Pi to the latest version, now we can build recalbox-buildroot again.

--- package/rgb-pi/rgb-pi.mk
+++ package/rgb-pi/rgb-pi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RGB_PI_VERSION = 02ff5a085e8aa641c76677059904c48bf02ec226
+RGB_PI_VERSION = 78c481aaa8ca558ddaf591bfb05e03b56ea19fbb
 #RGB_PI_SITE = git://github.com/mortaca/RGB-Pi
 RGB_PI_SITE = $(call github,mortaca,rgb-pi,$(RGB_PI_VERSION))
 RGB_PI_DEPENDENCIES += rpi-firmware
